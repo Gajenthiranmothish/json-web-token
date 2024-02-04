ab58449dc5cb9c61a5e6a68a3b5a96eefb105d46
gajenthiran mothish <gajenthiranmothish@gmail.com>
2024-02-04 23:08:31 +0530

    [JWT] - Minor changes

--- src/main/java/com/gajenthiranmothish/jsonwebtoken/config/SecurityConfig.java
+++ src/main/java/com/gajenthiranmothish/jsonwebtoken/config/SecurityConfig.java
@@ -15,6 +15,9 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -48,7 +51,10 @@ public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        DelegatingPasswordEncoder delPasswordEncoder=  (DelegatingPasswordEncoder)PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        NoOpPasswordEncoder noOpPasswordEncoder = (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+        delPasswordEncoder.setDefaultPasswordEncoderForMatches(noOpPasswordEncoder);
+        return delPasswordEncoder;
     }
 
     @Bean
